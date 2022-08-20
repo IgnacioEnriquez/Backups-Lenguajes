413262c5311770e2d604d0f83fd0e65e14add439
IgnacioEnriquez <ignacioenriquez009@gmail.com>
2022-08-20 13:05:55 -0300

    Backup Javascript Creado

---
+++ Backup-JavaScript/Clases/Clase1/js/codigo.js
@@ -0,0 +1,18 @@
+//Cometario de una sola linea
+/* Comentario de 
+varias lineas */
+/*
+alert("Mensaje desde codigo externo");
+
+//Variables
+var num = 120;
+var apellido = "NN"
+var numeroDecimal = 3.14;
+var encendido = true;
+
+//Output(salidas en JS)
+document.write("Busqueda de clientes");
+document.write("<h2>Busqueda de clientes</h2>");*/
+
+
+
