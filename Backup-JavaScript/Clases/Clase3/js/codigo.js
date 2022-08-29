92344955a1b5f97f192c7f251dfe4570a03ec2a3
IgnacioEnriquez <ignacioenriquez009@gmail.com>
2022-08-28 22:36:17 -0300

    Backups Actualizados

---
+++ Backup-JavaScript/Clases/Clase3/js/codigo.js
@@ -0,0 +1,29 @@
+// var fecha = new Date();
+// var dia = fecha.getDay();
+// new Date(año, mes , dia);
+// alert(dia);
+
+// Bucles
+// Mientras(While)
+
+// while(condicion)
+// {
+//     cuerpo del ciclo
+// }
+
+// var contador = 6;
+
+// while(contador<= 5)
+// {
+//     document.write("Cantidad actual : " + contador);
+// }
+
+// document.write("Ciclo Detenido");
+
+var contador = 1;
+var texto = ""
+
+do{
+    texto = texto + contador;
+    contador++;
+} while(contador <=5);
---
+++ Backup-JavaScript/Clases/Clase4/js/codigo.js
@@ -0,0 +1,57 @@
+
+// // document.getElementById("empleados").innerHTML = empleados[ultimo];
+// // document.getElementById("datos").innerHTML = datos[1];
+
+// // var persona = ["Rodolfo", "NN", "San Martin 1330"];
+
+// // Objetos (clave:valor)
+// // var persona = {nombre : "Rodolfo", apellido : "NN", edad : 52, direccion: "San Martin 130"};
+// // alert(persona.nombre);
+
+// // Arreglo de Objetos
+// // var personas = [{nombre:"Rodolfo", edad: 52},{nombre:"Emily", edad: 35}];
+// // alert("Nombre: " + personas[1].nombre + ", edad" + personas[1].edad);
+
+// // var empleados = ["Marcela", "Juan", "Emilio", "Ana"];
+
+// // PUSH(agrega un elemento al final)
+
+// // empleados.push("Pablo");
+
+// // POP(Elimina el ult elemento)
+
+// // empleados.pop();
+
+// // UNSHIFT(Agrega un elemento al principio)
+
+// // empleados.unshift("Sofia");
+
+// // SHIFT(Quita el primer elemento)
+
+// // empleados.shift();
+
+// // SPLICE(Indica cuantos elementos quiero eliminar, y por cual lo quiero reemplazar);
+
+// // Remueve desde el elemento 2, el segundo argumento indica cuantos elementos remueve, y el tercero si lo queiro reemplazar)
+
+// // empleados.splice(2,1,"Lucas");
+
+// CONCAT(Concatena dos arrays o mas)
+// var felinos = ["Leon", "Tigre"];
+// var reptiles = ["Vibora", "Lagarto"];
+// var zoo = felinos.concat(reptiles,empleados);
+
+// SLICE(Parte un arreglo desde cierta posicion)
+
+// var nombres = ["Juan", "Marcelo", "Mario", "Maria", "Elena"];
+
+// var chicas = nombres.slice(3);
+
+// MATRIZ
+
+// var personas_mtrx = [["Macarena","Lopez", 45],
+//                      ["Sofia","Gonzalez", 36]
+//                      ["Ricardo","Gonzalez", 24]]
+
+
+
